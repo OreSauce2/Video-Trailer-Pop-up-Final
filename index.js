967d239a0077578cb3a902d51d0d544296e67900
OreSauce2 <130902086@live.unilag.edu.ng>
2022-11-19 21:13:52 +0100

    Commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const btnEL = document.querySelector('.btn');
+const closeEl = document.querySelector('.close-icon');
+const trailerContainerEl = document.querySelector('.trailer-container');
+const videoEl = document.querySelector('video');
+
+btnEL.addEventListener('click', () => {
+    trailerContainerEl.classList.remove('active');
+});
+
+closeEl.addEventListener('click', () => {
+    trailerContainerEl.classList.add('active');
+    videoEl.pause();
+    videoEl.currentTime = 0;
+});
